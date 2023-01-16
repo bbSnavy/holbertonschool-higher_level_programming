70c8b5d2c4e53f168ef7339397910050a24cbf81
snavy <1337snavy@gmail.com>
2023-01-16 17:30:14 +0000

    ex06

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-l = []
-for x in range(10):
-    for y in range(x, 10):
-        l.append('%s%s' % (x, y))
-print(', '.join(l) + 0 * '{:02}'.format(0))
+print(', '.join(list(dict([(''.join(sorted(set(list(v)), key=int)), v)\
+for v in sorted([v for v in ['{:02}'.format(i) for i in range(100)] if\
+ (lambda a: len(set(list(a))) == 2)(v)], key=int)]).keys())))
