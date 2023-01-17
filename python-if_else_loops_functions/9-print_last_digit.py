acff34bd317cceec7d489a96a33b93b9d14561be
snavy <1337snavy@gmail.com>
2023-01-17 15:54:07 +0000

    ex09

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    try:
-        r = str(int(str(number)))[-1]
-        print(r, end='')
-        return r
-    except:
-        return ''
+    r = str(int(str(number)))[-1]
+    print(r, end='')
+    return r
