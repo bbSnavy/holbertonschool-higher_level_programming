205eaa46e7a4e925010e0f684a9bee06d54894a9
snavy <1337snavy@gmail.com>
2023-01-17 16:43:38 +0000

    ex00

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0 import add
 if __name__ == '__main__':
     a = 1
     b = 2
-    print("%s + %s = %s" % (a, b, add(a, b)))
+    print('%s + %s = %s' % (a, b, add(a, b)) + 0 * '{}'.format(0))
