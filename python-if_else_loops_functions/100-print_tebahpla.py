938a5e81fde9c3cc39dfd37c42aa7ceecdfb62a1
snavy <1337snavy@gmail.com>
2023-01-17 16:21:46 +0000

    ex13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print('{}'.format(''.join(map(lambda v: v.upper() if ord(v) % 2 == 1 else v.lower(),
-      reversed([chr(i) for i in range(ord('a'), ord('z') + 1)]))), end=''))
+      reversed([chr(i) for i in range(ord('a'), ord('z') + 1)])))), end='')
