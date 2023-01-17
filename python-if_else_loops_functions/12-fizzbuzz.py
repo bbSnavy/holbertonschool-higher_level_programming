813347a7fefef4b9ef77e72a6cc66a796c053143
snavy <1337snavy@gmail.com>
2023-01-17 16:16:35 +0000

    ex12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz(i=1):
     if i > 100:
-        print()
         return
-    k = '%d%d' % (i % 5 == 0, i % 3 == 0)
-    v = {
-        '00': str(i),
-        '01': 'Fizz',
-        '10': 'Buzz',
-    }.get(k, 'FizzBuzz')
-    print(v, end=' ')
+    print({'00': str(i), '01': 'Fizz', '10': 'Buzz'}
+          .get('%d%d' % (i % 5 == 0, i % 3 == 0), 'FizzBuzz'), end=' ')
     fizzbuzz(i + 1)
