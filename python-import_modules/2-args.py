9e11a5a8af52645a68c79a428ace4a34741bf716
snavy <1337snavy@gmail.com>
2023-01-17 16:55:36 +0000

    ex02

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,11 @@ import sys
 
 if __name__ == '__main__':
     length = len(sys.argv) - 1
-    print('{} argument'.format(length) + 's' * int(length != 1) + ':')
-    for i, v in enumerate(sys.argv):
-        if i == 0:
-            continue
-        print('{}: {}'.format(i, v))
+    if length == 0:
+        print('0 arguments.')
+    else:
+        print('{} argument'.format(length) + 's' * int(length != 1) + ':')
+        for i, v in enumerate(sys.argv):
+            if i == 0:
+                continue
+            print('{}: {}'.format(i, v))
