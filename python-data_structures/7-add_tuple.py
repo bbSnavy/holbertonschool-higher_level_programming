566ba2d25784ca73358fada1fc625e6adf28854d
snavy <1337snavy@gmail.com>
2023-01-27 07:49:59 +0100

    ex07

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    x, y = 0, 0
+    if len(tuple_a) >= 1:
+        x = tuple_a[0]
+    if len(tuple_b) >= 1:
+        y = tuble_b[0]
+    return (x, y)
