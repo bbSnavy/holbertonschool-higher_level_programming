f906b2abb609ba0536e2bcc7022cb9aadaf203ae
snavy <1337snavy@gmail.com>
2023-01-27 15:34:38 +0000

    ex01

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,4 +4,4 @@
 def search_replace(my_list, search, replace):
     if my_list is None:
         return None
-    return [v for v in my_list if v != search else replace]
+    return [v if v != search else replace for v in my_list]
