4eab387e0a53ba8dffce016a31a73b21239a3080
snavy <1337snavy@gmail.com>
2023-01-27 16:54:33 +0000

    ex04

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,6 +12,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print('out of range')
         finally:
-            r.append(v)
+            r.append(float(v))
 
     return r
