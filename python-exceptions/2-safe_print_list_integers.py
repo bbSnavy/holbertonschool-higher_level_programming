a84810a240938f315d9cc77e5dc7c232ff54deee
snavy <1337snavy@gmail.com>
2023-01-27 16:44:14 +0000

    ex02

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -11,6 +11,9 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             continue
 
+        if v is None:
+            continue
+
         if type(v) != int:
             continue
 
