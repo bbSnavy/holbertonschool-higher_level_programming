a9350cd0fcefe4d2c97fa6f9578d983d00da46d9
snavy <1337snavy@gmail.com>
2023-01-27 16:14:36 +0000

    ex12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 
-
 def roman_to_int(roman_string):
     if roman_string is None:
         return None
     if type(roman_string) is not str:
         return None
+    s = roman_string.upper()
     r = 0
-    for c in roman_string:
-        r += {
-            'i': 1,
-            'v': 5,
-            'x': 10,
-            'l': 50,
-            'c': 100,
-            'd': 500,
-            'm': 1000,
-        }.get(c.lower(), 0)
+    for i, _ in enumerate(roman_string):
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+
+        if i != (len(roman_string) - 1) and d[s[i]] < d[s[i + 1]]:
+            r -= d[s[i]]
+        else:
+            r += d[s[i]]
+
     return r
