a6aa3854a3e60e4fff99fbb3a5c022000b25ce74
snavy <1337snavy@gmail.com>
2023-01-27 16:31:08 +0000

    ex00

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,8 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print(my_list[i], end='')
+            if my_list[i] is None:
+                continue
+            print("{:d}".format(my_list[i]), end='')
         except IndexError:
             pass
