657d464d29f56ee2b47d4ba0370ac15c0359ae0c
snavy <1337snavy@gmail.com>
2023-01-27 16:47:17 +0000

    ex03

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        return a / b
+    except:
+        return None
+    finally:
+        if b == 0:
+            print('Inside result: None')
+        else:
+            print('Inside result: %s' % (a / b))
