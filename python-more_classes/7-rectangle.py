93da4fa39fffc3ae21bf35c21366702fbad883ca
snavy <1337snavy@gmail.com>
2023-02-03 16:07:29 +0100

    ex07

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle():
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ''
-        return '\n'.join([Rectangle.print_symbol * self.width for _ in range(self.height)])
+        return '\n'.join([self.print_symbol * self.width for _ in range(self.height)])
 
     def __repr__(self):
         return 'Rectangle(%s, %s)' % (self.width, self.height)
