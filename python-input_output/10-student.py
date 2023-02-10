cddbf3980741ba2c8a2835ca840fd9950d468e11
snavy <1337snavy@gmail.com>
2023-02-10 01:44:37 +0000

    ex10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" student """
+
+
+class Student():
+    """ student """
+
+    def __init__(self, first_name='', last_name='', age=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        r = {}
+        for k, v in self.__dict__.items():
+            f = True
+            if attrs is not None:
+                f = False
+            if type(attrs) is list:
+                for attr in attrs:
+                    if k == attr:
+                        f = True
+            else:
+                f = True
+
+            if not f:
+                continue
+
+            if not isinstance(v, (list, dict, str, int, bool)):
+                raise TypeError(
+                    "Attribute {} is not serializable".format(attr))
+            r[k] = v
+        return r
