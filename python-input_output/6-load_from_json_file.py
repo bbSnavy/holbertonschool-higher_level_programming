b8740066cde64a8bff2f4f06b614ab0e3432984e
snavy <1337snavy@gmail.com>
2023-02-10 01:25:03 +0000

    ex05

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ save json file """
     with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+        json.dump(my_obj, filename)
