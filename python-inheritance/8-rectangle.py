615b65dee49705d5a244da54a799437aa780a191
snavy <1337snavy@gmail.com>
2023-02-10 13:25:51 +0100

    ex08

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,11 +6,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ rectangle """
 
-    width = 0
-    height = 0
-
     def __init__(self, width, height):
         self.integer_validator('width', width)
         self.integer_validator('height', height)
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
