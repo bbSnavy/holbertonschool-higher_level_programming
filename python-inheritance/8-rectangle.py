e03cd7b40c59439f1f188bbb050da0b51fac78bf
snavy <1337snavy@gmail.com>
2023-02-10 11:48:35 +0100

    ex08

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,18 +2,20 @@
 """ rectangle """
 
 
+def integer_validator(self, name, value):
+    """ integer validator """
+    if type(value) not in [int]:
+        raise TypeError("{} must be an integer".format(name))
+
+    if value <= 0:
+        raise ValueError("{} must be greater than 0".format(name))
+
+    return value
+
+
 class Rectangle():
     """ rectangle """
 
     def __init__(self, width, height):
         self._Rectangle__width = self.integer_validator("width",  width)
         self._Rectangle__height = self.integer_validator("height", height)
-
-    def integer_validator(self, name, value):
-        if type(value) not in [int]:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-        return value
