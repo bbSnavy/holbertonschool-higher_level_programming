7a5e393ea25a0b5773cb6d46f791287149875064
snavy <1337snavy@gmail.com>
2023-02-10 13:51:51 +0100

    ex10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" square """
+""" Base Geometry """
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ square """
+    """ Base Geometry """
 
     def __init__(self, size):
         self.integer_validator('size', size):
