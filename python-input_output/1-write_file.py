b2aa438731dac02a5c468c559f086c860b16eade
snavy <1337snavy@gmail.com>
2023-02-10 01:20:13 +0000

    ex01

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" write file """
+
+
+def write_file(filename="", text=""):
+    """ write file """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
