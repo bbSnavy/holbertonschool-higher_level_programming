90b505a9ca901c4f3d9f4bfa6a9b3af5f6658e3c
snavy <1337snavy@gmail.com>
2023-02-10 01:39:36 +0000

    ex08

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ class to json """
-import json
 
 
 def class_to_json(obj):
     """ class to json """
-    return json.loads(json.dumps(obj))
+    r = {}
+    for k, v in obj.__dict__.items():
+        if not isinstance(v, (list, dict, str, int, bool)):
+            raise TypeError("Attribute {} is not serializable".format(attr))
+        r[k] = v
+    return r
