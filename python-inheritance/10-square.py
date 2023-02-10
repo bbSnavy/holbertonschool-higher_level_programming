594ee8e6c8f6ceab5836430ca38ed9a5c48a9d1a
snavy <1337snavy@gmail.com>
2023-02-10 14:58:25 +0100

    ex10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
     """ Base Geometry """
 
     def __init__(self, size):
-        self.integer_validator('size', size):
         self.__size = size
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
-        return self.__size * self.__size
+        return self.__size ** 2
