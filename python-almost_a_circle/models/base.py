e616e182f55fc7bd0edace5356c3cc2b09b01a7b
snavy <1337snavy@gmail.com>
2023-02-14 14:36:17 +0100

    ex16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """ save to file """
         l = []
