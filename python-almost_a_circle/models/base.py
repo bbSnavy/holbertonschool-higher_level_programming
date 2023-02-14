2ae2e996fa94719e17cb895924ba24fe529d4e5e
snavy <1337snavy@gmail.com>
2023-02-14 14:25:50 +0100

    ex16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,3 +21,8 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """ save to file """
+        with open('%s.json' % (cls.__name__), 'w') as file:
+            file.write(Base.to_json_string(list_objs))
