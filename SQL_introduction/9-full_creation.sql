7f0a3ee4a2a52ec6d56c96882120fc6a7ecc17a4
snavy <1337snavy@gmail.com>
2023-03-18 22:04:23 +0000

    ex09

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- full creation
+CREATE TABLE IF NOT EXISTS second_table (
+	id	int,
+	name	VARCHAR(256),
+	score	int
+);
+
+INSERT INTO second_table VALUES (1, "John", 10);
+INSERT INTO second_table VALUES (2, "Alex", 10);
+INSERT INTO second_table VALUES (3, "Bob", 10);
+INSERT INTO second_table VALUES (4, "George", 10);
