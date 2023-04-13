ba557bfbb257c59730ab0ad1dba5a4398d84b427
snavy <1337snavy@gmail.com>
2023-04-13 13:57:55 +0000

    ex02

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.args === undefined) {
   console.log('No argument');
-} else if (process.args.length === 1) {
+} else if (process.args.length === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
