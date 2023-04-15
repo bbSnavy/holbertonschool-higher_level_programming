4f947131b2acc027c528999f4e389fb0b77f836d
snavy <1337snavy@gmail.com>
2023-04-15 21:05:45 +0000

    ex06

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareOld = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareOld {
   constructor (size) {
     super(size, size);
   }
