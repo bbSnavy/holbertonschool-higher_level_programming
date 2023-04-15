510a4e05e9366037915e3f68d7c4bf013154c350
snavy <1337snavy@gmail.com>
2023-04-15 20:51:18 +0000

    ex03

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || h === undefined) {
+      return;
+    }
+
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
